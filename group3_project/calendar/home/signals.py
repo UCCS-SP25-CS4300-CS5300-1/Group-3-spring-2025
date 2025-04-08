97fe5193b9c43d56bc63f09bae555137f0fc9bad
abarrag4 <alebarr564@gmail.com>
2025-04-08 04:31:16 +0000

    Fix 3

--- group3_project/calendar/home/signals.py
+++ group3_project/calendar/home/signals.py
@@ -3,7 +3,6 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-@csrf_exempt
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
