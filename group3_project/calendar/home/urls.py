cde5064dddba8631126a8f9b240912d16e807355
ashleeparadis <ashlee.paradis@outlook.com>
2025-05-04 23:12:17 +0000

    test fail fixed

--- group3_project/calendar/home/urls.py
+++ group3_project/calendar/home/urls.py
@@ -1,10 +1,8 @@
 #urls.py
-from django.urls import path
-from .views import (
-    index, fetch_assignments, calendar_view, clear_calendar,
-    courses_list, course_detail, assignment_detail,
-    wipe_saved, add_event, user_settings, register
-)
+from django.urls import path, include
+from .views import *
+from django.contrib.auth import views as auth_views
+from . import views as home_views
 
 urlpatterns = [
     path('', index, name='index'),
