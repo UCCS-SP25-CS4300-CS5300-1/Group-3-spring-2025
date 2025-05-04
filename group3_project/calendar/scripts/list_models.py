c1188e2fd00f1ebcef865612f1ddab4797bbbb44
Aidan-Meshberg <113375912+Aidan-Meshberg@users.noreply.github.com>
2025-05-04 15:50:25 -0600

    Update list_models.py

--- group3_project/calendar/scripts/list_models.py
+++ group3_project/calendar/scripts/list_models.py
@@ -1,1 +1,10 @@
+import os
+from openai import OpenAI
 
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+models = client.models.list()
+
+print("Available models:")
+for model in models.data:
+    print("-", model.id)
